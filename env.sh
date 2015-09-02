65885bdb29ebc535a49ba902afb8ff96c344952f
Peter Bryzgalov <peterbryz@yahoo.com>
2015-09-02 18:40:18 +0900

    Edited README: restructured for easy workflow understanding. env.sh and env.inc do not include TAU_MAKEFILE

--- env.sh
+++ env.sh
@@ -4,4 +4,3 @@ export TAU_ARCH=sparc64fx
 export TAU=$TAUDIR/$TAU_ARCH/lib
 export TAUROOTDIR=$TAUDIR
 export PATH="$TAUDIR/$TAU_ARCH/bin:$TAUDIR/x86_64/bin:$PATH"
-export TAU_MAKEFILE="$TAU/Makefile.tau-mpi-pdt-fujitsu"
